9e433d80e110691b2d25b6e2efa71a1807039219
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-02 20:53:10 -0300

    att js

--- JS/Aula 03/Aula 03- JS/Funções/main.js
+++ JS/Aula 03/Aula 03- JS/Funções/main.js
@@ -1,1 +1,20 @@
 //Estrutura de uma função.
+/* 
+function NomeDaFuncao(valor1, valor2){
+    return valor1 * valor2
+}
+*/
+function AddNums(num1 = 2, num2 = 5) {
+    return num1 + num2;
+}
+
+// console.log(AddNums());
+let x = AddNums(10, 11);
+console.log(x);
+console.clear();
+
+// Criando Arrow Funcion
+const hello = () => {
+    return 'Olá mundo';
+};
+console.log(hello());
---
+++ JS/Aula 03/Exercicio Lógica/js ex 2/media.js
@@ -0,0 +1,7 @@
+let Ana = 8;
+let Joao = 9;
+let Maria = 7;
+let Pedro = 6;
+let mediaTurma = (Maria + Pedro + Joao + Ana) / 4;
+console.log(mediaTurma);
+//A média da turma é 7,5 , portanto esta acima da média escolar
