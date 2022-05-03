1a1808d7100c1dd031e7ac2ac3470be6973ec316
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-02 21:57:53 -0300

    aula js 03

--- JS/Aula 03/Aula 03- JS/Funções/main.js
+++ JS/Aula 03/Aula 03- JS/Funções/main.js
@@ -7,6 +7,9 @@ function NomeDaFuncao(valor1, valor2){
 function AddNums(num1 = 2, num2 = 5) {
     return num1 + num2;
 }
+const somaAddNums = (num1 = 2, num2 = 5) => {
+    return num1 + num2;
+};
 
 // console.log(AddNums());
 let x = AddNums(10, 11);
@@ -17,4 +20,22 @@ console.clear();
 const hello = () => {
     return 'Olá mundo';
 };
+// Retornar resultado da função
 console.log(hello());
+
+//Criando Objetos
+const aluno = {
+    primeiroNome: 'Cassio',
+    segundoNome: 'Chagas',
+    idade: '20',
+};
+console.clear();
+//Evento onload mostra algo enquanto carrega a Página WEB
+
+const BoasVindas = () => {
+    alert('Bem vindo a nossa pagina');
+};
+console.log('Bem vindo a nossa pagina');
+function EventClick() {
+    console.log('Você apertou o botão');
+}
