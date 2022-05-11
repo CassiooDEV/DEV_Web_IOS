befebe6965c52dbc3a08f16de945783c89f67be5
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-11 18:19:01 -0300

    ex js 05

---
+++ JS/Aula 05/Exercicio 05/main.js
@@ -0,0 +1,10 @@
+let leroLero =
+    'Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.';
+console.log(leroLero[0]);
+console.log(`O tamanho da string é: ${leroLero.length}`);
+let sub = leroLero.replace('Dr Stephen Strange', 'Feiticeira Escarlate');
+console.log(sub);
+let sep = leroLero.split(' ');
+console.log(sep);
+console.log(sep[13]);
+console.log(leroLero.toUpperCase());
