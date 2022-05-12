4ba6f4974be8dced96098c12a54f3d69ef1d67b7
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-12 20:55:21 -0300

    ex js 08

---
+++ JS/Aula 08/Exercicio 08/main.js
@@ -0,0 +1,17 @@
+let c = 0;
+let tab = 2;
+let i2 = 0;
+console.log('MÉTODO WHILE');
+while (c <= 10) {
+    console.log(`O resultado de ${tab} x ${c} é : ${tab * c}`);
+    c++;
+}
+console.log('MÉTODO FOR');
+for (let i = 0; i <= 10; i++) {
+    console.log(`O resultado de ${tab} x ${i} é : ${tab * i}`);
+}
+console.log('MÉTODO DO-WHILE');
+do {
+    console.log(`O resultado de ${tab} x ${i2} é : ${tab * i2}`);
+    i2++;
+} while (i2 <= 10);
---
+++ JS/Aula 08/main.js
@@ -0,0 +1,18 @@
+// let profHelo = 1;
+// while (profHelo <= 5) {
+//     console.log(`Bate palma`);
+//     profHelo++;
+// }
+// do {
+//     console.log('Bate palma');
+//     profHelo++;
+// } while (profHelo <= 5);
+// //Decremento
+// let contador = 10;
+// while (contador >= 0) {
+//     console.log(`O valor do contador é ${contador}`);
+//     contador--;
+// }
+for (let contador2 = 10; contador2 >= 0; contador2--) {
+    console.log(`O valor do contador é ${contador2}`);
+}
