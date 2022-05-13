89a1fe758d1e40d24b216e987da67d59e87333dc
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-12 21:32:40 -0300

    ex sala

--- JS/Aula 08/main.js
+++ JS/Aula 08/main.js
@@ -16,3 +16,23 @@
 for (let contador2 = 10; contador2 >= 0; contador2--) {
     console.log(`O valor do contador é ${contador2}`);
 }
+//Laços de repetição e Array
+const frutas = ['maca', 'laranja', 'pera', 10];
+for (let j = 0; j < frutas.length; j++) {
+    console.log(`Nome: ${frutas[j]}`);
+}
+
+//Crie um array chamado carros com 8 modelos e percorra o array de 2 em 2
+const carros = [
+    'bmw',
+    'ford',
+    'ferrari',
+    'sedan',
+    'bugatti',
+    'volks',
+    'HB20',
+    'fiat',
+];
+for (let c = 0; c < carros.length; c += 2) {
+    console.log(`Modelo do carro: ${carros[c]}`);
+}
