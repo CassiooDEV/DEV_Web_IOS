d9eb4d5e261954d0d5a2f502d25455a4118a4d57
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-27 01:42:42 -0300

    ex 15 80%

--- JS/Aula 15/Exercicio 15/JS/main.js
+++ JS/Aula 15/Exercicio 15/JS/main.js
@@ -2,6 +2,8 @@ let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
 let decrease = document.getElementsByClassName('decrease');
 let lista = document.getElementById('items');
+let carrinho = document.getElementById('compra');
+let div = document.getElementById('total');
 form.addEventListener('submit', addItem);
 function addItem(e) {
     e.preventDefault();
@@ -12,9 +14,7 @@ function addItem(e) {
         let li = document.createElement('li');
         li.className = 'list-group-item';
         let text = document.createTextNode(
-            `Item: ${newItemText} Valor Unitário:R$${newItemvalue},00 Valor:R$${
-                newItemvalue * newItemqntd
-            },00 Quantidade: ${newItemqntd}`
+            `Item: ${newItemText} Valor Unitário:R$${newItemvalue},00`
         );
         li.appendChild(text);
         let deleteBtn = document.createElement('button');
@@ -31,23 +31,43 @@ function addItem(e) {
         decreaseBtn.appendChild(document.createTextNode('-'));
         li.appendChild(decreaseBtn);
         items.appendChild(li);
+        let li2 = document.createElement('li');
+        li2.className = 'list-group-item';
+        li2.appendChild(
+            document.createTextNode(`${newItemqntd}x ${newItemText} `)
+        );
+        carrinho.appendChild(li2);
+        let h5 = document.createElement('h5');
+        h5.className = 'total';
+        h5.appendChild(document.createTextNode('Valor'));
+        carrinho.appendChild(h5);
+        let p = document.createElement('p');
+        p.className = 'total';
+        p.appendChild(
+            document.createTextNode(`R$${newItemvalue * newItemqntd},00`)
+        );
+        carrinho.appendChild(p);
+        deleteBtn.addEventListener('click', excluir);
+        function excluir() {
+            li.remove();
+            li2.remove();
+            h5.remove();
+            p.remove();
+            deleteBtn.remove();
+        }
+        addBtn.addEventListener('click', add);
+        function add() {
+            let add = newItemqntd++;
+            li2.innerHTML = `${add}x ${newItemText}`;
+            p.innerHTML = `R$${newItemvalue * add},00`;
+        }
+        decreaseBtn.addEventListener('click', decrease);
+        function decrease() {
+            let dec = newItemqntd--;
+            li2.innerHTML = `${dec}x ${newItemText}`;
+            p.innerHTML = `R$${newItemvalue * dec},00`;
+        }
     } else {
         alert('Valor e quantidade deve ser maior que 0!');
     }
 }
-function addBtn() {
-    let text = document.getElementById('item1').value;
-    let valor = document.getElementById('item2').value;
-    let qntd = document.getElementById('item3').value;
-    let li = document.getElementsByClassName('list-group-item');
-    li.appendChild(
-        document.createTextNode(
-            `Item: ${text} Valor: R$${valor * qntd + valor},00 Quantidade: ${
-                qntd + 1
-            }`
-        )
-    );
-    items.appendChild(li);
-}
-add = document.getElementsByClassName('add');
-add.addEventListener('click', addBtn);
