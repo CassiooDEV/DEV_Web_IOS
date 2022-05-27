b92bb56ad73329a2fedcf541b362b4dfb9630778
CassiooDEV <cassiochagas2013@gmail.com>
2022-05-26 21:45:01 -0300

    ex 15 inc

--- JS/Aula 15/Exercicio 15/JS/main.js
+++ JS/Aula 15/Exercicio 15/JS/main.js
@@ -1,49 +1,53 @@
-const valores = [18, 12, 15];
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
-let add = document.getElementsByClassName('add');
 let decrease = document.getElementsByClassName('decrease');
-let remove = document.getElementsByClassName('delete');
-let carrinho = document.getElementById('compra');
+let lista = document.getElementById('items');
 form.addEventListener('submit', addItem);
-add.addEventListener('click', addItem);
-filter.addEventListener('keyup', buscarItems);
-
 function addItem(e) {
     e.preventDefault();
-    let li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.innerHTML = 'Arroz';
-    li.appendChild(document.createTextNode(newItem));
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn- float-end delete';
-    deleteBtn.appendChild(document.createTextNode('x'));
-    li.appendChild(deleteBtn);
-    carrinho.appendChild(li);
-    form.reset();
-}
-// Função Remove Item
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        let li = e.target.parentElement;
-        itemList.removeChild(li);
+    let newItemText = document.getElementById('item1').value;
+    let newItemvalue = document.getElementById('item2').value;
+    let newItemqntd = document.getElementById('item3').value;
+    if (newItemvalue > 0 && newItemqntd > 0) {
+        let li = document.createElement('li');
+        li.className = 'list-group-item';
+        let text = document.createTextNode(
+            `Item: ${newItemText} Valor Unitário:R$${newItemvalue},00 Valor:R$${
+                newItemvalue * newItemqntd
+            },00 Quantidade: ${newItemqntd}`
+        );
+        li.appendChild(text);
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'btn btn-danger btn- float-end btns delete';
+        deleteBtn.appendChild(document.createTextNode('x'));
+        li.appendChild(deleteBtn);
+        let addBtn = document.createElement('button');
+        addBtn.className = 'btn btn-primary btn-sm float-end btns add';
+        addBtn.appendChild(document.createTextNode('+'));
+        li.appendChild(addBtn);
+        let decreaseBtn = document.createElement('button');
+        decreaseBtn.className =
+            'btn btn-primary btn-sm float-end btns decrease';
+        decreaseBtn.appendChild(document.createTextNode('-'));
+        li.appendChild(decreaseBtn);
+        items.appendChild(li);
+    } else {
+        alert('Valor e quantidade deve ser maior que 0!');
     }
 }
-// Função buscarItems
-function buscarItems(e) {
-    // Converte o texto digitado para minúsculo
-    let text = e.target.value.toLowerCase();
-    // Busca todos os itens
-    let items = itemList.getElementsByTagName('li');
-    // Converte os itens para array
-    Array.from(items).forEach(function (item) {
-        // Busca o primeiro item da lista
-        let itemName = item.firstChild.textContent;
-        // Busca o item na lista que começa com o mesmo texto digitado
-        if (itemName.toLowerCase().indexOf(text) != -1) {
-            item.style.display = 'block'; // exibe o item
-        } else {
-            item.style.display = 'none'; // oculta o item
-        }
-    });
+function addBtn() {
+    let text = document.getElementById('item1').value;
+    let valor = document.getElementById('item2').value;
+    let qntd = document.getElementById('item3').value;
+    let li = document.getElementsByClassName('list-group-item');
+    li.appendChild(
+        document.createTextNode(
+            `Item: ${text} Valor: R$${valor * qntd + valor},00 Quantidade: ${
+                qntd + 1
+            }`
+        )
+    );
+    items.appendChild(li);
 }
+add = document.getElementsByClassName('add');
+add.addEventListener('click', addBtn);
