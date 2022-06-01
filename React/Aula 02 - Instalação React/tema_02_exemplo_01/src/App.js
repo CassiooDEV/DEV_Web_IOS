9a759941f3b39cbe485a499812df4c14341ae767
CassiooDEV <cassiochagas2013@gmail.com>
2022-06-01 20:23:36 -0300

    react

---
+++ React/Aula 02 - Instalação React/tema_02_exemplo_01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <p>Essa é a minha primeira aplicação React</p>
+        </div>
+    );
+}
+
+export default App;
