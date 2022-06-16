d6b6039dd6f7943be004fc39deca094eca71fc66
CassiooDEV <cassiochagas2013@gmail.com>
2022-06-16 20:47:11 -0300

    ex java 02

---
+++ Java/02_TiposOperadores/Exercicio_02/src/CalcArea.java
@@ -0,0 +1,7 @@
+public class CalcArea {
+    public static void main (String[] args){
+        double base = 7.0, height = 9.0,tarea = 0;
+        tarea = (base * height)/2;
+        System.out.println("A area do triangulo retangulo -> "+tarea);
+    }
+}
---
+++ Java/02_TiposOperadores/Exercicio_02/src/CalcMedia.java
@@ -0,0 +1,7 @@
+public class CalcMedia {
+    public static void main(String[] args){
+        double nota1 = 5.5,nota2 = 9.7,nota3 = 7.4,nota4 = 6.8,media = 0;
+        media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println("A média do aluno é: "+media);
+    }
+}
