1aa9616c2256694528d8a68a6c099e3515f942c8
CassiooDEV <cassiochagas2013@gmail.com>
2022-06-27 19:54:42 -0300

    att java

--- Java/07_TesteDeMesa/Exercicio/src/br/com/aluno/Banco.java
+++ Java/07_TesteDeMesa/Exercicio/src/br/com/aluno/Banco.java
@@ -3,15 +3,18 @@ package br.com.aluno;
 import java.util.Scanner;
 
 public class Banco {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Scanner input = new Scanner(System.in);
         char resp = 's';
 
         while (resp == 's' || resp == 'S') {
-            System.out.println("======= SETBank ========");
+            System.out.println("======= Bem-Vindo ao SETBank ========");
+            Thread.sleep(3000);
+            System.out.println("======= Qual seu tipo de conta? ========");
             System.out.println(" 1 - Conta Corrente ");
             System.out.println(" 2 - Conta Poupança ");
             System.out.println(" 3 - Conta Estudante ");
+            Thread.sleep(2000);
             System.out.println("======= Digite o número correpondente á conta que deseja ========");
             System.out.print("-->");
             int menu = input.nextInt();
